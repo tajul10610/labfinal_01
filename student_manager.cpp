0cfa4b188a34abcf0da2378b37eba1754ffc0b1d
tajul10610 <tajulislam10610@gmail.com>
2025-04-15 15:30:43 +0600

    create class student manager

--- student_manager.cpp
+++ student_manager.cpp
@@ -1,36 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
-
-#ifdef UNIT_TEST
-#include <gtest/gtest.h>
-#endif
 
 class StudentManager {
     std::vector<std::string> students;
 
 public:
-    void add(const std::string& name) {
+    void addStudent(const std::string& name) {
         students.push_back(name);
     }
 
-    bool remove(const std::string& name) {
-        auto it = std::find(students.begin(), students.end(), name);
-        if (it != students.end()) {
-            students.erase(it);
-            return true;
+    void removeStudent(const std::string& name) {
+        for (size_t i = 0; i < students.size(); ++i) {
+            if (students[i] == name) {
+                students.erase(students.begin() + i);
+                break;
+            }
         }
-        return false;
     }
-
-    void show() const {
-        std::cout << "Students:\n";
-        for (const auto& s : students)
-            std::cout << "- " << s << "\n";
-    }
-
-    std::vector<std::string> list() const { return students; }
-
-    void clear() { students.clear(); } // for test reset
-};
