de845e94ccac353e3fdfeed7df6a1e88d5ebae04
tajul10610 <tajulislam10610@gmail.com>
2025-04-15 10:04:16 +0600

    initial commit

---
+++ text.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "gtest/gtest.h"
+
+// === Utility functions to test ===
+
+int square(int x) {
+    return x * x;
+}
+
+bool is_even(int x) {
+    return x % 2 == 0;
+}
+
+// === Google Test Cases ===
+
+TEST(SquareFunction, HandlesPositiveInput) {
+    EXPECT_EQ(square(5), 25);
+}
+
+TEST(SquareFunction, HandlesZero) {
+    EXPECT_EQ(square(0), 0);
+}
+
+TEST(SquareFunction, HandlesNegativeInput) {
+    EXPECT_EQ(square(-3), 9);
+}
+
+TEST(IsEvenFunction, HandlesEvenInput) {
+    EXPECT_TRUE(is_even(10));
+}
+
+TEST(IsEvenFunction, HandlesOddInput) {
+    EXPECT_FALSE(is_even(7));
+}
+
+// === Main Function ===
+
+int main(int argc, char **argv) {
+    std::cout << "Running math_utils tests..." << std::endl;
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
